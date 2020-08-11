32fe4e5c82ba6b227169ed58cc836b5523e4b1b7
Hunter Eisler <heisler98@gmail.com>
2020-08-10 23:58:45 -0500

    Initial Commit

---
+++ Rhythmic/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Rhythmic
+//
+//  Created by Hunter Eisler on 8/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rhythmic/RhythmicApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmicApp.swift
+//  Rhythmic
+//
+//  Created by Hunter Eisler on 8/10/20.
+//
+
+import SwiftUI
+
+@main
+struct RhythmicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
